89d7420f821a557fcb41e5d0d68188b30b458a38
EmmaZhang <emmazxd@gmail.com>
2023-12-02 14:24:14 +0800

    Use the inquirer npm package to get user input.

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 /* 
-1. Use the inquirer npm package to get user input.
+
 2. Use the qr-image npm package to turn the user entered URL into a QR code image.
 3. Create a txt file to save the user input using the native fs node module.
 */
 import inquirer from 'inquirer';
 
 inquirer
-  .prompt([
-    {"messsage":"Type your URL:",
+  .prompt([{
+    messsage:"Type your URL:",
+    //"URL" is the object name
     name:"URL"
   }])
   .then((answers) => {
-    // Use user feedback for... whatever!!
+    // 1. Use the inquirer npm package to get user input.
+    const url = answers.URL;
   })
   .catch((error) => {
     if (error.isTtyError) {
