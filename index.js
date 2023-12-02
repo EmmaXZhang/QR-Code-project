698e08970b08e2f8f20c2e735817f1700865b5e9
EmmaZhang <emmazxd@gmail.com>
2023-12-02 14:42:38 +0800

    using the native fs to create txt file

--- index.js
+++ index.js
@@ -1,29 +1,35 @@
-/* 
 
 
-3. Create a txt file to save the user input using the native fs node module.
-*/
 import inquirer from 'inquirer';
 import qr from "qr-image";
 import fs from "fs";
 
 inquirer
-  .prompt([{
+  .prompt([
+    {
     messsage:"Type your URL:",
     //"URL" is the object name
     name:"URL"
-  }])
+  },
+])
   .then((answers) => {
     // 1. Use the inquirer npm package to get user input.
     const url = answers.URL;
     // 2. Use the qr-image npm package to turn the user entered URL into a QR code image.
     var qr_svg=qr.image(url);
     qr_svg.pipe(fs.createWriteStream("qr_img.png"));
+    // 3. Create a txt file to save the user input using the native fs node module.
+    fs.writeFile("URL.txt", url, (err) => {
+        if (err) throw err;
+        console.log("The file has been saved!");
+      });
+
+
   })
   .catch((error) => {
     if (error.isTtyError) {
-      // Prompt couldn't be rendered in the current environment
+
     } else {
-      // Something else went wrong
+ 
     }
   });
